cc4cac3f874ac795c2825d776a3f9df17e2b8518
elegantcoder <david@elegantcode.com>
2015-03-14 19:55:30 -0700

    added PrimeFactors03

---
+++ prime-factors/PrimeFactors03/PrimeFactors03/PrimeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFactors03
+{
+    public class PrimeGenerator
+    {
+        public int[] GetPrimes(int numToFactor)
+        {
+            var primes = new List<int>();
+            var candidate = 2;
+
+            while (numToFactor > 1)
+            {
+                while (numToFactor%candidate == 0)
+                {
+                    primes.Add(candidate);
+                    numToFactor /= candidate;
+                }
+                candidate++;
+            }
+
+            return primes.ToArray();
+        }
+    }
+}
