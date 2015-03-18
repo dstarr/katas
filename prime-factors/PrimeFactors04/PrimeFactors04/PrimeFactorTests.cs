7821a65735c1c958357afbe3fbc7317f85e72ba0
elegantcoder <david@elegantcode.com>
2015-03-18 10:54:49 -0700

    adding completed Prime Factors Kata 4

---
+++ prime-factors/PrimeFactors04/PrimeFactors04/PrimeFactorTests.cs
@@ -0,0 +1,64 @@
+﻿using Xunit;
+
+namespace PrimeFactors04
+{
+    public class PrimeFactorTests
+    {
+        private PrimeGenerator _generator;
+
+        public PrimeFactorTests()
+        {
+            _generator = new PrimeGenerator();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int[] factors = _generator.Factor(1);
+            Assert.Equal(new int[]{}, factors);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] factors = _generator.Factor(2);
+            Assert.Equal(new int[] { 2 }, factors);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] factors = _generator.Factor(3);
+            Assert.Equal(new int[] { 3 }, factors);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int[] factors = _generator.Factor(4);
+            Assert.Equal(new int[] { 2, 2 }, factors);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            int[] factors = _generator.Factor(6);
+            Assert.Equal(new int[] { 2, 3 }, factors);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            int[] factors = _generator.Factor(8);
+            Assert.Equal(new int[] { 2, 2, 2 }, factors);
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            int[] factors = _generator.Factor(9);
+            Assert.Equal(new int[] { 3, 3 }, factors);
+        }
+
+    }
+}
---
+++ prime-factors/PrimeFactors04/PrimeFactors04/PrimeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFactors04
+{
+    public class PrimeGenerator
+    {
+        internal int[] Factor(int numToFactor)
+        {
+            var factors = new List<int>();
+            var candidateFactor = 2;
+
+            while (numToFactor > 1)
+            {
+                while (numToFactor%candidateFactor == 0)
+                {
+                    factors.Add(candidateFactor);
+                    numToFactor /= candidateFactor;
+                }
+                candidateFactor++;
+            }
+
+            return factors.ToArray();
+        }
+    }
+}
