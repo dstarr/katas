c1cc591716ce6b546c4f9b3a01f34fd1ae8eb2c5
elegantcoder <david@elegantcode.com>
2015-03-18 14:10:17 -0700

    committing PrimeFactors05

---
+++ PrimeFactors05/PrimeFactors05/PrimeFactorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PrimeFactors05
+{
+
+    public class PrimeFactorTests
+    {
+        private PrimeGenerator _generator;
+
+        public PrimeFactorTests()
+        {
+            _generator = new PrimeGenerator();
+        }
+
+        [Fact]
+        public void Testing1()
+        {
+            int[] factors = _generator.Factor(1);
+            Assert.Equal(new int[]{}, factors);
+        }
+
+        [Fact]
+        public void Testing2()
+        {
+            int[] factors = _generator.Factor(2);
+            Assert.Equal(new int[] { 2 }, factors);
+        }
+
+        [Fact]
+        public void Testing3()
+        {
+            int[] factors = _generator.Factor(3);
+            Assert.Equal(new int[] { 3 }, factors);
+        }
+
+        [Fact]
+        public void Testing4()
+        {
+            int[] factors = _generator.Factor(4);
+            Assert.Equal(new int[] { 2, 2 }, factors);
+        }
+
+        [Fact]
+        public void Testing6()
+        {
+            int[] factors = _generator.Factor(6);
+            Assert.Equal(new int[] { 2, 3 }, factors);
+        }
+
+        [Fact]
+        public void Testing8()
+        {
+            int[] factors = _generator.Factor(8);
+            Assert.Equal(new int[] { 2, 2, 2 }, factors);
+        }
+
+        [Fact]
+        public void Testing9()
+        {
+            int[] factors = _generator.Factor(9);
+            Assert.Equal(new int[] { 3, 3 }, factors);
+        }
+
+        [Fact]
+        public void Testing100()
+        {
+            int[] factors = _generator.Factor(100);
+            Assert.Equal(new int[] { 2, 2, 5, 5 }, factors);
+        }
+    }
+}
---
+++ PrimeFactors05/PrimeFactors05/PrimeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeFactors05
+{
+    class PrimeGenerator
+    {
+        internal int[] Factor(int numToFactor)
+        {
+            var factors = new List<int>();
+
+            for (int candidate = 2; numToFactor > 1; candidate++)
+            {
+                for (; numToFactor%candidate == 0; numToFactor/= candidate)
+                {
+                    factors.Add(candidate);
+                }
+            }
+            
+            return factors.ToArray();
+        }
+    }
+}
