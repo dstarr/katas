94e4fd7357f13af8a6dc67567177b6c8acad2eec
elegantcoder <david@elegantcode.com>
2015-03-22 16:55:00 -0700

    adding PrimeFactors07

---
+++ PrimeFactors07/PrimeFactors07/PrimeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFactors07
+{
+    public class PrimeGenerator
+    {
+        public int[] Factor(int numToFactor)
+        {
+            List<int> factors = new List<int>();
+            var factorCandidate = 2;
+
+            while (numToFactor > 1)
+            {
+                while (numToFactor%factorCandidate == 0)
+                {
+                    factors.Add(factorCandidate);
+                    numToFactor /= factorCandidate;
+                }
+                factorCandidate++;
+            }
+
+            return factors.ToArray();
+        }
+    }
+}
---
+++ PrimeFactors07/PrimeFactors07/PrimeGeneratorTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PrimeFactors07
+{
+    [TestFixture]
+    public class PrimeGeneratorTests
+    {
+        private PrimeGenerator _generator;
+
+        [SetUp]
+        public void Init()
+        {
+            _generator = new PrimeGenerator();
+        }
+
+
+        [Test]
+        public void Testing1()
+        {
+            int[] primes = _generator.Factor(1);
+            Assert.AreEqual(new int[] {}, primes);
+        }
+
+        [Test]
+        public void Testing2()
+        {
+            int[] primes = _generator.Factor(2);
+            Assert.AreEqual(new int[] { 2 }, primes);
+        }
+
+        [Test]
+        public void Testing3()
+        {
+            int[] primes = _generator.Factor(3);
+            Assert.AreEqual(new int[] { 3 }, primes);
+        }
+
+        [Test]
+        public void Testing4()
+        {
+            int[] primes = _generator.Factor(4);
+            Assert.AreEqual(new int[] { 2, 2 }, primes);
+        }
+
+        [Test]
+        public void Testing6()
+        {
+            int[] primes = _generator.Factor(6);
+            Assert.AreEqual(new int[] { 2, 3 }, primes);
+        }
+
+        [Test]
+        public void Testing8()
+        {
+            int[] primes = _generator.Factor(8);
+            Assert.AreEqual(new int[] { 2, 2, 2 }, primes);
+        }
+
+        [Test]
+        public void Testing9()
+        {
+            _generator = new PrimeGenerator();
+            int[] primes = _generator.Factor(9);
+            Assert.AreEqual(new int[] { 3, 3 }, primes);
+        }
+    }
+}
