db89e64ee22b72eb8bc9a1b86ed3fe87ac39c18c
David Starr <dxstarr@godaddy.com>
2016-02-16 14:49:08 -0800

    Bowling 14 in Java

---
+++ bowling/Bowling14/test/GameSpecs.java
@@ -0,0 +1,68 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by dxstarr on 2/16/2016.
+ */
+public class GameSpecs {
+
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
+    @Test
+    public void allGutterballs() {
+
+        int timesRolled = 20;
+        int pinsDown = 0;
+
+        rollManyTimes(pinsDown, timesRolled);
+
+        assertEquals(0, game.getScore());
+    }
+
+    @Test
+    public void allOnes() {
+
+        rollManyTimes(1, 20);
+
+        assertEquals(20, game.getScore());
+    }
+
+    @Test
+    public void rollSpare() {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+
+        assertEquals(16, game.getScore());
+    }
+
+    @Test
+    public void rollSrike() {
+        game.roll(10);
+        game.roll(5);
+        game.roll(3);
+
+        assertEquals(26, game.getScore());
+    }
+
+    @Test
+    public void perfectGame() {
+        rollManyTimes(10, 12);
+
+        assertEquals(300, game.getScore());
+    }
+
+    private void rollManyTimes(int pinsDown, int timesRolled) {
+        for (int i = 0; i < timesRolled; i++) {
+            game.roll(pinsDown);
+        }
+    }
+
+}
