dea191ab37ecb23103673d91452c92561be89a8b
David Starr <david@elegantcode.com>
2025-01-26 11:23:58 -0800

    Initial commit

---
+++ prime-factors/PrimeFactors09/PrimeFactors.Tests/PrimeGeneratorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic;
+
+namespace PrimeFactors.Tests;
+
+[TestClass]
+public class PrimeGeneratorTests
+{
+    private PrimeGenerator _primeGenerator = null!;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _primeGenerator = new PrimeGenerator();
+    }
+    
+    [TestMethod]
+    public void NotPrime_1()
+    {
+        var factors = _primeGenerator.FactorToPrimes(1);
+        Assert.AreEqual(0, factors.Length);
+    }
+    
+    [TestMethod]
+    public void Prime_2()
+    {
+        int[] results = _primeGenerator.FactorToPrimes(2);
+        CollectionAssert.AreEqual(new int[] { 2 }, results);
+    }
+    
+    [TestMethod]
+    public void Prime_4()
+    {
+        var results = _primeGenerator.FactorToPrimes(4);
+        CollectionAssert.AreEqual(new[] { 2, 2 }, results);
+    }
+}
---
+++ prime-factors/PrimeFactors09/PrimeFactors/PrimeGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace PrimeFactors;
+
+public class PrimeGenerator
+{
+    public int[] FactorToPrimes(int numToFactor)
+    {
+        var factors = new List<int>();
+        var factorCandidate = 2;
+
+        while (numToFactor > 1)
+        {
+            while (numToFactor % factorCandidate == 0)
+            {
+                factors.Add(factorCandidate);
+                numToFactor /= factorCandidate;
+            }
+            factorCandidate++;
+        }
+
+        return factors.ToArray();
+    }
+}
